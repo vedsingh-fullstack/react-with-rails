cd03830381c3dbec7254ca3649e8675be8456f21
Vedprakash <ved.singh303@hotmail.com>
2016-09-28 11:35:27 +0530

    Added React to rails app.

---
+++ app/assets/javascripts/comments.js
@@ -0,0 +1,9 @@
+var Comments = React.createClass({
+  render: function() {
+    return React.DOM.div({
+      className: 'records'
+    }, React.DOM.h2({
+      className: 'title'
+    }, 'Comments List'));
+  }
+});
